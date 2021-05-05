4cb61f8d89a63dbf8465e7408dec9d5bdc97f439
yolandagb <yolandagarciabarranco@hotmail.com>
2021-05-05 11:13:52 +0000

    finisheeeed

--- src/app.js
+++ src/app.js
@@ -6,15 +6,33 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-  console.log("Hello Rigo from the console!");
+  function generateExcuse() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car", "my food"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "while I was praying"
+    ];
+    let positionWho = Math.floor(Math.random() * 4);
+
+    let positionAction = Math.floor(Math.random() * 4);
+
+    let positionWhat = Math.floor(Math.random() * 4);
+
+    let positionWhen = Math.floor(Math.random() * 5);
+
+    let excuse =
+      who[positionWho] +
+      " " +
+      action[positionAction] +
+      " " +
+      what[positionWhat] +
+      " " +
+      when[positionWhen];
+    document.getElementById("excuse").innerHTML = excuse;
+  }
+  generateExcuse();
 };
